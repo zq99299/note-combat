b13175a120ef7574ef2b4c624a0423b9685d0479
zq99299 <99299684@qq.com>
2021-01-11 16:29:12 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,70 @@
+const springSc = require('../spring')
+
+module.exports = {
+  // ~ 开发配置
+  dest: 'build/.vuepress/dist',  // 目录配置在外,纯粹是有代码洁癖和强迫症，并不能规避开发模式下同时构建不报错的问题
+  host: 'localhost', // dev 的域名
+  port: 8080,
+
+  // ~ 站点配置
+  title: '实战笔记',
+  description: '主要定位是在工作中实际遇到的难题、没有接触过、或稍有难度的真实场景解决记录',
+  base: '/note-combat/', // gh-page 中是增加了项目名
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    docsDir: 'docs',
+    sidebar: 'auto',
+    sidebarDepth: 3, // 嵌套标题侧边栏提取深度，最大为 2，会提取到 h3
+    lastUpdated: '上次更新', // string | boolean
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'https://github.com/zq99299/note-combat',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: 'GitHub',
+    docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'main',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！',
+    // 主题级别的配置
+    serviceWorker: {
+      updatePopup: true // Boolean | Object, 默认值是 undefined.
+    },
+    nav: [
+      { text: 'Spring 实战技巧', link: '/spring/' }
+    ],
+    sidebar: {
+      '/spring/': springSc()
+    }
+  },
+  plugins: [
+    ['@vuepress/back-to-top', true],
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: {
+        message: '有新内容更新啦~',
+        buttonText: '立即获取新内容，确定后稍后自动刷新'
+      }
+    }],
+    ['@vuepress/medium-zoom', {
+      selector: '.theme-default-content img'
+    }],
+    ['@vuepress/search', {
+      searchMaxSuggestions: 10
+    }],
+    ['vuepress-plugin-code-copy', true],
+    ['@vuepress/last-updated', {
+      transformer: (timestamp, lang) => {
+        // 不要忘了安装 moment
+        const moment = require('moment')
+        return moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
+      }
+    }],
+    ['vuepress-plugin-tags'],
+    ['vuepress-plugin-baidu-autopush', true]
+  ]
+}
